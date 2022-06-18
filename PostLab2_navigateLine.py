7fb9254483b25abaae54265327c04082b808d691
Romeo <107235113+RomeoBlancia@users.noreply.github.com>
2022-06-19 03:38:34 +0800

    Update PostLab2_navigateLine.py

--- PostLab2_navigateLine.py
+++ PostLab2_navigateLine.py
@@ -1,10 +1,10 @@
-filename = input("Please enter the file name: ")    #prompts for the file name 
-f = open(filename, 'r')     #opens the file in read mode
-l = f.readlines()           #all the lines are added to the list l
-while(1):                   #enters into the loop
-    print("There are", len(l), "lines in file.")    #displays no.of lines in file
-    n = int(input("Please enter the line number you want: "))   #prompts for the line number
-    if(n == 0):             #if n is zeror
-        break               #break from the loop and quit the program
-    else:                   #else
-        print(l[n-1])       #prints that particular line(as list index starts from 0, we took n-
+filename = input("Please enter the file name: ")    
+f = open(filename, 'r')    
+l = f.readlines()           
+while(1):                  
+    print("There are", len(l), "lines in file.")   
+    n = int(input("Please enter the line number you want: "))   
+    if(n == 0):            
+        break               
+    else:                   
+        print(l[n-1])      
