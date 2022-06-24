c691aac1a08d48d98859f2e6da1381231c0b072c
Romeo <107235113+RomeoBlancia@users.noreply.github.com>
2022-06-24 18:16:53 +0800

    Add files via upload

---
+++ stats.py
@@ -0,0 +1,28 @@
+def mode(lyst):
+    lyst.sort() 
+    list_elem = dict()
+    for i in lyst:
+        list_elem[i] = list_elem.get(i, 0) + 1
+    
+    return max(list_elem, key=list_elem.get)
+
+def median(lyst):
+    lyst.sort() 
+    return lyst[int(len(lyst)/2)]
+
+def mean(lyst):
+    sum=0 
+    
+    for i in range(0,len(lyst)):
+        sum=sum+lyst[i] 
+    return sum/len(lyst)
+
+def main():
+    lyst=[1,24,11,50,21,69,35]
+    print("List:",lyst)
+    print("Mode:",mode(lyst))
+    print("Median:",median(lyst))
+    print("Mean:",mean(lyst))
+
+if __name__=="__main__":
+    main() 
